0b12ef1ce0779379e74eb2099cc94d39469dfda6
brianngeywo <brianngeywo254@gmail.com>
2023-08-18 01:44:09 +0300

    added get_it and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:news_reminder_app/homepage.dart';
 import 'package:news_reminder_app/package_model.dart';
 import 'package:news_reminder_app/scraper_service.dart';
 
+import 'firebase_options.dart';
 import 'http_service.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
